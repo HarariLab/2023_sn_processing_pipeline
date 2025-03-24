bd7be67d3285ae381de507f34ac2c5523687b8f6
aladyeva-research <ekaterina.aladyeva@osumc.edu>
2025-03-24 13:42:26 -0400

    Made cell cycle scores optional

--- scripts/integration_sct.R
+++ scripts/integration_sct.R
@@ -30,13 +30,17 @@ seu_clean <- SCTransform(
 
 ## CELL CYCLE SCORING
 
-s.genes <- stringr::str_to_title(cc.genes$s.genes)
-g2m.genes <- stringr::str_to_title(cc.genes$g2m.genes)
+if ("run_cell_cycle_scores" %in% names(snakemake@config)) {
+  if (snakemake@config[["run_cell_cycle_scores"]]) {
+    s.genes <- stringr::str_to_title(cc.genes$s.genes)
+    g2m.genes <- stringr::str_to_title(cc.genes$g2m.genes)
 
 
-seu_clean <- CellCycleScoring(seu_clean, s.features = s.genes,
+    seu_clean <- CellCycleScoring(seu_clean, s.features = s.genes,
                               g2m.features = g2m.genes, set.ident = FALSE,
                               assay = "SCT")
+  }
+}
 saveRDS(seu_clean, file = snakemake@output[[1]])
 
 sink()
